3d1dda5facbf509368f1b8df11b48d30a53f1dab
klik0121 <dru0121@gmail.com>
2016-09-09 14:52:56 +0300

    Маленькое исправление грамматики.

--- ExpertSystemShell/Parsers/Grammars/ProductionModel/ProductionQueryGrammar.cs
+++ ExpertSystemShell/Parsers/Grammars/ProductionModel/ProductionQueryGrammar.cs
@@ -9,11 +9,19 @@ using System.Text.RegularExpressions;
 
 namespace ExpertSystemShell.Parsers.Grammars.ProductionModel
 {
+    /// <summary>
+    /// Грамматика для запросов к продукционной базе данных.
+    /// <para />Синтаксис: если #список фактов# то #запросы фактов#.
+    /// <para />#список фактов# задаётся следующим образом: один или несколько фактов через запятую.
+    /// <para />#факт# задаётся следующим образом: 'имя факта - значение факта'.
+    /// <para />#запросы фактов# задаются следющим образом: один или несколько элементарных запросов фактов.
+    /// <para />#элементарный запрос факта# задаётся следующим образом: имя факта =?.
+    /// </summary>
+    /// <seealso cref="Diggins.Jigsaw.SharedGrammar" />
     public class ProductionQueryGrammar: SharedGrammar
     {
         public static Rule IfKeyword = MatchRegex(new Regex("если", RegexOptions.IgnoreCase));
-        public static Rule If = Node(WS + IfKeyword + WS + ProductionActionGrammar.ProductionAction + 
-            ZeroOrMore(WS + Comma + WS + ProductionActionGrammar.ProductionAction));
+        public static Rule If = Node(WS + IfKeyword + WS + ProductionActionGrammar.ProductionActionList);
         public static Rule ThenKeyword = MatchRegex(new Regex("то", RegexOptions.IgnoreCase));
         public static Rule FactName = Node(Pattern(@"(\w)(\s*\w)*(?=\s*=\?)"));
         public static Rule GetOp = MatchString("=?");
