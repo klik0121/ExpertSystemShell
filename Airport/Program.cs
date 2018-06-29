31fbff01491ee839d45629bfd7aa8545c67410d0
klik0121 <dru0121@gmail.com>
2018-06-29 18:52:04 +0300

    some debug messages

--- Airport/Program.cs
+++ Airport/Program.cs
@@ -28,10 +28,14 @@ namespace Airport
             IParser parser = new PrModelParser(eh);
             IExpertSystem expert = new ExpertSystemBase(knBase, solver, parser);
             string rules = Properties.Resources.TravelBase;
+            Console.WriteLine("Parsing rules...");
             expert.AddRules(rules);
+            Console.WriteLine("Done!");
             string query = "если 'выбор направления-Европа', 'выбор страны-Испания', 'приоритет отеля-центр города', 'приоритет города-экскурсии', 'количество звезд-4' то 'отель=?'";
             //string query = "если 'желание-купить', 'денег-достаточно', 'расстояние-равно', " +
             //    "'цены лучше-РИО' то 'пойти в РИО=?', 'пойти в Рубин=?'";
+            Console.WriteLine("Submitting query: {0}", query);
+            Console.Write("Result:");
             ResultingFactSet result = (ResultingFactSet)expert.GetResult(query);
             foreach (var item in result)
             {
